fca59c23c417f8f570981503803f74eb58e66bef
denvolkov <denvolkov@inbox.ru>
2020-12-14 14:54:42 +0300

    selected bottom segment

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -15,12 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomLabel: UILabel!
     @IBOutlet weak var bottomSegmentControl: UISegmentedControl!
     
-    var topChoices = [CaptionOption]()
+    var topChoices: [CaptionOption] = []
+    var bottomChoices: [CaptionOption] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTopSegmentControl()
+        configureBottomSegmentControl()
+        selectInitValues()
+        
     }
     
     func configureTopSegmentControl() {
@@ -38,11 +42,36 @@ class ViewController: UIViewController {
         
     }
     
+    func configureBottomSegmentControl() {
+        bottomSegmentControl.removeAllSegments()
+        
+        let option1 = CaptionOption(emoji: "🐱", text: "Cats wearing hats")
+        let option2 = CaptionOption(emoji: "🐶", text: "Dogs carrying logs")
+        let option3 = CaptionOption(emoji: "🙈", text: "Monkeys being funky")
+        bottomChoices = [option1, option2, option3]
+        
+        for choice in bottomChoices {
+            bottomSegmentControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+            
+        }
+    }
+    
+    func selectInitValues() {
+        topCaptionSegmentControl.selectedSegmentIndex = 0
+        bottomSegmentControl.selectedSegmentIndex = 0
+        topLabel.text = topChoices.first?.text
+        bottomLabel.text = bottomChoices[0].text
+    }
+    
     @IBAction func topSegmentValueChanged(_ sender: UISegmentedControl) {
         let currentChoice = topChoices[sender.selectedSegmentIndex]
         topLabel.text = currentChoice.text
     }
     
+    @IBAction func bottomSegmentValueChanged(_ sender: UISegmentedControl) {
+        let currentChoice = bottomChoices[sender.selectedSegmentIndex]
+        bottomLabel.text = currentChoice.text
+    }
     
 }
 
