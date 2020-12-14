c7e06ba864b3c448434d9285b5d41f695d280f8c
denvolkov <denvolkov@inbox.ru>
2020-12-14 12:32:43 +0300

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by test on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
